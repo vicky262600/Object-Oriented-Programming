fe2426090028864d976d79c3a0bc424bf5d9f3e3
vicky262600 <vickyjoshi2604@gmail.com>
2024-02-17 14:36:21 -0500

    getter and setter

--- GettersAndSettersItem6.py
+++ GettersAndSettersItem6.py
@@ -6,6 +6,7 @@ class Item:
     def __init__(self, name: str, price: float, quantity=1):
         assert price > 0, f"Price {price} is not greater than or equal to 0"
 
+
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -53,34 +54,3 @@ class Item:
 
 
 Item.form_csv()
-# for instance in Item.all:
-#     print(instance.name, instance.price, instance.quantity)
-    
-# print(Item.all)
-# print(Item.is_integer(10.5));
-
-
-
-class Phone(Item):
-    #  pay_rate = 0.8
-     all = []
-     def __init__(self, name: str, price: float, quantity=1, brokenPhone=0):
-        # assert price >= 0, f"Price {price} is not greater than or equal to 0"
-        # assert brokenPhone >= 0, f"brokenPhone {brokenPhone} is not greater than or equal to 0"
-        # call to super function to have access to all attribue/ methode
-        super().__init__(
-            name, price, quantity
-        )
-
-        # self.name = name
-        # self.price = price
-        # self.quantity = quantity
-        self.brokenPhone = brokenPhone
-
-
-phone1 = Phone("iphone11", 1000, 2, 1)
-# print(phone1.calculate_total_price())
-phone2 = Phone("iphone12", 1100, 3)
-
-print(Item.all)
-# print(Phone.all)
---
+++ main6.py
@@ -0,0 +1,7 @@
+# from GettersAndSettersphone6 import Phone
+from GettersAndSettersItem6 import Item
+
+item1 = Item("shirt", 25)
+item1.name = "pant"
+
+print(item1.name)
